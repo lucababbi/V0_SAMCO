5e01b2cb06571033f14d05db808efed49822ad8d
lbabbi@qontigo.com <lbabbi@qontigo.com>
2024-09-25 23:11:50 +0200

    "ToBeContinued"

--- Review_Process.py
+++ Review_Process.py
@@ -1,5 +1,11 @@
 import polars as pl
 import pandas as pd
+from datetime import date
+
+##################################
+###########Parameters#############
+##################################
+Starting_Date = date(1997, 3, 24)
 
 ##################################
 #Read Developed/Emerging Universe#
@@ -29,19 +35,78 @@ Emerging = pl.read_parquet(r"C:\Users\lbabbi\OneDrive - ISS\Desktop\Projects\SAM
                             pl.col("Date").cast(pl.Date)
                             ])
 
+##################################
+######Add Cutoff Information######
+##################################
+Columns = ["validDate", "stoxxId", "currency", "closePrice", "shares"]
+
+# Read the Parquet and add the Review Date Column 
+Securities_Cutoff = pl.read_parquet(r"C:\Users\lbabbi\OneDrive - ISS\Desktop\Projects\SAMCO\V0_SAMCO\Securities_Cutoff\Securities_Cutoff.parquet", columns=Columns).with_columns([
+                      pl.col("closePrice").cast(pl.Float64),
+                      pl.col("shares").cast(pl.Float64),
+                      pl.col("validDate").cast(pl.Utf8).str.strptime(pl.Date, "%Y%m%d")
+                      ]).join(pl.read_csv(r"C:\Users\lbabbi\OneDrive - ISS\Desktop\Projects\SAMCO\V0_SAMCO\Dates\Review_Date-QUARTERLY.csv").with_columns(
+                        pl.col("Review").cast(pl.Utf8).str.strptime(pl.Date, "%m/%d/%Y"),
+                        pl.col("Cutoff").cast(pl.Utf8).str.strptime(pl.Date, "%m/%d/%Y")
+                      ), left_on="validDate", right_on="Cutoff", how="left")
+
+# Add these information to the Developed and Emerging Universes
+Developed = (
+    Developed
+    .join(
+        Securities_Cutoff,
+        left_on=["Date", "Internal_Number"],
+        right_on=["Review", "stoxxId"],
+        how="left"
+    )
+    .with_columns([
+        # Fill null values in "currency" with the values from "Currency"
+        pl.col("currency").fill_null(pl.col("Currency")),
+    ])
+    .drop("Currency")  # Drop the "Currency" column after filling nulls
+    .rename({
+        "validDate": "Cutoff",
+        "closePrice": "Close_unadjusted_local_Cutoff",
+        "shares": "Shares_Cutoff",
+        "currency": "Currency"
+    })
+).join(pl.read_parquet(r"C:\Users\lbabbi\OneDrive - ISS\Desktop\Projects\SAMCO\V0_SAMCO\Securities_Cutoff\FX_Cutoff.parquet").with_columns(
+   pl.col("Cutoff").cast(pl.Date)), left_on=["Cutoff", "Currency"], right_on=["Cutoff", "frm_currency"], how="left")
+
+Emerging = (
+    Emerging
+    .join(
+        Securities_Cutoff,
+        left_on=["Date", "Internal_Number"],
+        right_on=["Review", "stoxxId"],
+        how="left"
+    )
+    .with_columns([
+        # Fill null values in "currency" with the values from "Currency"
+        pl.col("currency").fill_null(pl.col("Currency")),
+    ])
+    .drop("Currency")  # Drop the "Currency" column after filling nulls
+    .rename({
+        "validDate": "Cutoff",
+        "closePrice": "Close_unadjusted_local_Cutoff",
+        "shares": "Shares_Cutoff",
+        "currency": "Currency"
+    })
+)
+
 # Calculate Free/Full MCAP USD for Developed Universe
 Developed = Developed.with_columns(
-                                    (pl.col("Free_Float") * pl.col("Capfactor") * pl.col("Close_unadjusted_local") * pl.col("FX_local_to_Index_Currency") * pl.col("Shares"))
-                                    .alias("Free_Float_MCAP_USD"),
-                                    (pl.col("Close_unadjusted_local") * pl.col("FX_local_to_Index_Currency") * pl.col("Shares") / (pl.col("Capfactor") * pl.col("Free_Float")))
-                                    .alias("Full_MCAP_USD")
+                                    (pl.col("Free_Float") * pl.col("Capfactor") * pl.col("Close_unadjusted_local_Cutoff") * pl.col("FX_local_to_Index_Currency_Cutoff") * pl.col("Shares_Cutoff"))
+                                    .alias("Free_Float_MCAP_USD_Cutoff"),
+                                    (pl.col("Close_unadjusted_local_Cutoff") * pl.col("FX_local_to_Index_Currency_Cutoff") * pl.col("Shares_Cutoff") / (pl.col("Capfactor") * pl.col("Free_Float")))
+                                    .alias("Full_MCAP_USD_Cutoff")
                                   )
 
 # Calculate Free/Full MCAP USD for Emerging Universe
 Emerging = Emerging.with_columns(
-                                    (pl.col("Free_Float") * pl.col("Capfactor") * pl.col("Close_unadjusted_local") * pl.col("FX_local_to_Index_Currency") * pl.col("Shares"))
-                                    .alias("Free_Float_MCAP_USD"),
-                                    (pl.col("Close_unadjusted_local") * pl.col("FX_local_to_Index_Currency") * pl.col("Shares") / (pl.col("Capfactor") * pl.col("Free_Float")))
+                                    (pl.col("Free_Float") * pl.col("Capfactor") * pl.col("Close_unadjusted_local_Cutoff") * pl.col("FX_local_to_Index_Currency_Cutoff") * pl.col("Shares_Cutoff"))
+                                    .alias("Free_Float_MCAP_USD_Cutoff"),
+                                    (pl.col("Close_unadjusted_local_Cutoff") * pl.col("FX_local_to_Index_Currency_Cutoff") * pl.col("Shares_Cutoff") / (pl.col("Capfactor") * pl.col("Free_Float")))
                                     .alias("Full_MCAP_USD")
                                   )
 
@@ -77,3 +142,12 @@ for date in Developed["Date"].unique():
 
     GMSR_Frame = GMSR_Frame.vstack(New_Data)
 
+#################################
+##Start the Size Classification##
+#################################
+for date, country in Emerging.select(["Date", "Country"]).unique().sort(["Date", "Country"]).iter_rows():
+
+  if date == Starting_Date: # First Review Date where Standard Index is created
+    temp_Country = Emerging.filter((pl.col("Date") == date) & (pl.col("Country") == country))
+
+    # Sort in each Country the Companies by Full MCAP USD Cutoff
