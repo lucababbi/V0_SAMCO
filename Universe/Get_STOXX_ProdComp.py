7540c284e7a92fd35923c0521d38ec95d02d5ad3
lucababbi <lbabbi@qontigo.com>
2024-10-08 23:07:57 +0200

    "CapFactorExtended"

--- Universe/Get_STOXX_ProdComp.py
+++ Universe/Get_STOXX_ProdComp.py
@@ -165,7 +165,7 @@ for index in idx:
         Output = Output.astype(str)
         Output = pl.from_pandas(Output)
 
-    Output = Output.select(pl.col(["Date", "Internal_Number", "Instrument_Name", "Mcap_Units_Index_Currency"]))
+    Output = Output.select(pl.col(["Date", "Internal_Number", "Capfactor"]))
 
-    Output.write_parquet(rf"C:\Users\et246\Desktop\V0_SAMCO\Universe\{index}_Review_Extended.parquet")
+    Output.write_parquet(rf"C:\Users\et246\Desktop\V0_SAMCO\Universe\{index}_Capfactor_Extended.parquet")
     Output = pd.DataFrame()
